3f93b246f32cb2412e6fc14bdb8a0ba1525e7c4e
Cassiano Montanari <cassianomontanari@gmail.com>
2023-12-25 23:37:52 +0100

    change content 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ const App = () => {
   return (
     <>
       <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-          perspiciatis consequatur debitis beatae, adipisci aliquid laudantium.
+        <p
+          style={{
+            fontFamily: 'monospace',
+            fontSize: 16,
+            float: 'left',
+            marginLeft: 16,
+            marginTop: 16,
+          }}
+        >
+          cassmtnr
         </p>
-        <p>1234</p>
       </div>
     </>
   );
