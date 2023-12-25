c7818218ad395be0f08f9260be0673d78c322c97
Cassiano Montanari <cassianomontanari@gmail.com>
2023-12-25 13:55:54 +0100

    change content

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
           perspiciatis consequatur debitis beatae, adipisci aliquid laudantium.
         </p>
-        <p>2345</p>
+        <p>3456</p>
       </div>
     </>
   );
