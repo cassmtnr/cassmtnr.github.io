a9df292bb7584442c34f5fdf62f51a0054afabe9
Cassiano Montanari <cassianomontanari@gmail.com>
2023-12-25 14:06:42 +0100

    add lint

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
           perspiciatis consequatur debitis beatae, adipisci aliquid laudantium.
         </p>
-        <p>4567</p>
+        <p>5678</p>
       </div>
     </>
   );
