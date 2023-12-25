18f2a6cbe45fcb498cb5ab56b2e66768d91a489f
Cassiano Montanari <cassianomontanari@gmail.com>
2023-12-25 23:31:43 +0100

    fix test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ const App = () => {
     <>
       <div>
         <p>
-          Loarem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
           perspiciatis consequatur debitis beatae, adipisci aliquid laudantium.
         </p>
         <p>5678</p>
