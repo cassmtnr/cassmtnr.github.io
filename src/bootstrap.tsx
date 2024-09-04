0f1aa80576b650dd36da9f43d691e0a986f0ee81
Cassiano Montanari <cassianomontanari@gmail.com>
2024-09-04 23:46:17 +0200

    replace eslint and prettier with biome

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -1,4 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<App />);
+} else {
+  console.error('Root element not found');
+}
